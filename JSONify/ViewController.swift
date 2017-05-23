148f5a346455f86e0e189db01bc0e2bb76c8db25
Madhusudhan-BR <madhusudhanb.r@Madhusudhans-MacBook-Pro.local>
2017-05-23 09:49:53 -0400

    first commit parses JSON

--- JSONify/ViewController.swift
+++ JSONify/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlString = "http://jsonplaceholder.typicode.com/users"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.downloadFromUrl()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func downloadFromUrl() {
+        let url = NSURL(string: urlString)
+        
+        let task = URLSession.shared.dataTask(with: (url as? URL)!) { (data, response, error) in
+            //print(data)
+            if error != nil {
+                print("Error")
+            }
+            if let content = data {
+                
+                do{
+                    let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                    if let id = myJSON[0]
+                    {
+                        print(id)
+                    }
+                    
+                }
+                catch{
+                    
+                }
+                
+            }
+            
+            
+        }
 
+        task.resume()
+    
+    
 }
 
+}
